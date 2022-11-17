0eadcff6eec9e280ecabbdcda897f81b6b1ae1c8
Carlos Montero Marín <46814503+CarlosMonteroMarin@users.noreply.github.com>
2022-11-17 18:46:14 +0100

    Delete TA26_Spring_MySQL_EJ1 directory

--- TA26_Spring_MySQL_EJ1/src/main/java/com/TA26_EJ1/dao/IPiezaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.TA26_EJ1.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.TA26_EJ1.dto.Pieza;
-
-public interface IPiezaDAO extends JpaRepository<Pieza, Integer> {
-	
-}
--- TA26_Spring_MySQL_EJ1/src/main/java/com/TA26_EJ1/dao/IProveedoreDAO.java
+++
@@ -1,9 +0,0 @@
-package com.TA26_EJ1.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.TA26_EJ1.dto.Proveedor;
-
-public interface IProveedoreDAO extends JpaRepository<Proveedor, Integer>{
-
-}
