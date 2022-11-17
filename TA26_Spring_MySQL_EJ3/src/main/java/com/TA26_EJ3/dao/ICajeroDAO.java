8c7fc31f4ec512b298858e0f3228588398b6159f
Carlos Montero Marín <46814503+CarlosMonteroMarin@users.noreply.github.com>
2022-11-17 18:40:00 +0100

    Delete TA26_Spring_MySQL_EJ3 directory

--- TA26_Spring_MySQL_EJ3/src/main/java/com/TA26_EJ3/dao/ICajeroDAO.java
+++
@@ -1,9 +0,0 @@
-package com.TA26_EJ3.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.TA26_EJ3.dto.Cajero;
-
-public interface ICajeroDAO extends JpaRepository<Cajero, Integer> {
-	
-}
