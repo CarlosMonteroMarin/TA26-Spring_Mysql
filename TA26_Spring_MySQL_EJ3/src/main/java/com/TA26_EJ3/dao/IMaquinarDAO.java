0ec1fb3acdfbfcee1c8bfbdf89afa1ca41ff6d32
Carlos Montero Marín <46814503+CarlosMonteroMarin@users.noreply.github.com>
2022-11-17 18:46:29 +0100

    Delete TA26_Spring_MySQL_EJ3 directory

--- TA26_Spring_MySQL_EJ3/src/main/java/com/TA26_EJ3/dao/IMaquinarDAO.java
+++
@@ -1,9 +0,0 @@
-package com.TA26_EJ3.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.TA26_EJ3.dto.Maquinar;
-
-public interface IMaquinarDAO extends JpaRepository<Maquinar, Integer> {
-	
-}
