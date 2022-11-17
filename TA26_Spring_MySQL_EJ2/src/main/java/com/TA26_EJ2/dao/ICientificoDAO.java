96045061a3b251b56d8e9fed70d059a3e86638f7
Carlos Montero Marín <46814503+CarlosMonteroMarin@users.noreply.github.com>
2022-11-17 18:39:34 +0100

    Delete TA26_Spring_MySQL_EJ2 directory

--- TA26_Spring_MySQL_EJ2/src/main/java/com/TA26_EJ2/dao/ICientificoDAO.java
+++
@@ -1,8 +0,0 @@
-package com.TA26_EJ2.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.TA26_EJ2.dto.Cientifico;
-
-public interface ICientificoDAO extends JpaRepository<Cientifico, Long>{
-	
-}
